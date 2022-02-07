4ef21ccb15372b6053332f69625cf25e1d5fb25d
unknown <clarisa.lia96@gmail.com>
2022-02-07 08:12:44 +0700

    menambahkan file code

---
+++ script.js
@@ -0,0 +1,151 @@
+"use strict";
+
+function main() {
+  var canvas = document.querySelector("#canvas");
+  var gl = canvas.getContext("webgl");
+  if (!gl) {
+    return;
+  }
+
+  // set GLSL program 
+  var program = webglUtils.createProgramFromScripts(gl, ["vertex-shader-2d", "fragment-shader-2d"]);
+
+  // cari data vertex shader ada dimana
+  var positionLocation = gl.getAttribLocation(program, "a_position");
+
+  // cari uniforms
+  var resolutionLocation = gl.getUniformLocation(program, "u_resolution");
+  var colorLocation = gl.getUniformLocation(program, "u_color");
+  var translationLocation = gl.getUniformLocation(program, "u_translation");
+  var rotationLocation = gl.getUniformLocation(program, "u_rotation");
+  var scaleLocation = gl.getUniformLocation(program, "u_scale");
+
+  // Mebuat buffer untuk meletakkan informasi posisi 
+  var positionBuffer = gl.createBuffer();
+  // Menghubungkan dengan ARRAY_BUFFER
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  // meletakkan data geometry ke dalam buffer
+  setGeometry(gl);
+
+  var translation = [100, 150];
+  var rotation = [0, 1];
+  var scale = [1, 1];
+  var color = [Math.random(), Math.random(), Math.random(), 1];
+
+  drawScene();
+
+  // Set ui.
+  webglLessonsUI.setupSlider("#x", {value: translation[0], slide: updatePosition(0), max: gl.canvas.width });
+  webglLessonsUI.setupSlider("#y", {value: translation[1], slide: updatePosition(1), max: gl.canvas.height});
+  webglLessonsUI.setupSlider("#angle", {slide: updateAngle, max: 360});
+  webglLessonsUI.setupSlider("#scaleX", {value: scale[0], slide: updateScale(0), min: -5, max: 5, step: 0.01, precision: 2});
+  webglLessonsUI.setupSlider("#scaleY", {value: scale[1], slide: updateScale(1), min: -5, max: 5, step: 0.01, precision: 2});
+
+  function updatePosition(index) {
+    return function(event, ui) {
+      translation[index] = ui.value;
+      drawScene();
+    };
+  }
+
+  function updateAngle(event, ui) {
+    var angleInDegrees = 360 - ui.value;
+    var angleInRadians = angleInDegrees * Math.PI / 180;
+    rotation[0] = Math.sin(angleInRadians);
+    rotation[1] = Math.cos(angleInRadians);
+    drawScene();
+  }
+
+  function updateScale(index) {
+    return function(event, ui) {
+      scale[index] = ui.value;
+      drawScene();
+    };
+  }
+
+  // Fungsi gambar bentuk yang kita inginkan 
+  function drawScene() {
+    webglUtils.resizeCanvasToDisplaySize(gl.canvas);
+
+    // konversi clipspace menjadi pixel 
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+    // Membersihkan canvas
+    gl.clearColor(0.5, 0.6, 0.6, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+ 
+
+    // Menggunakan program yang telah kita deklarasikan di bagian atas
+    gl.useProgram(program);
+
+    // Mengaktifkan attribute
+    gl.enableVertexAttribArray(positionLocation);
+
+    // Mengghubungkan position buffer.
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+    // Mengambil data dari Position buffer
+    var size = 2;          // 2 components per iteration
+    var type = gl.FLOAT;   // the data is 32bit floats
+    var normalize = false; // don't normalize the data
+    var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+    var offset = 0;        // start at the beginning of the buffer
+    gl.vertexAttribPointer(
+        positionLocation, size, type, normalize, stride, offset);
+
+    // set resolusi
+    gl.uniform2f(resolutionLocation, gl.canvas.width, gl.canvas.height);
+
+    // set warna
+    gl.uniform4fv(colorLocation, color);
+
+    // Set translasi
+    gl.uniform2fv(translationLocation, translation);
+
+    // Set rotasi
+    gl.uniform2fv(rotationLocation, rotation);
+
+    // Set skala
+    gl.uniform2fv(scaleLocation, scale);
+
+    // Menggambar bentuk 
+    var primitiveType = gl.TRIANGLES;
+    var offset = 0;
+    var count = 18;  
+    gl.drawArrays(primitiveType, offset, count);
+  }
+}
+
+// informasi untuk menggambar bentuk F
+function setGeometry(gl) {
+  gl.bufferData(
+      gl.ARRAY_BUFFER,
+      new Float32Array([
+          // left column
+          0, 0,
+          30, 0,
+          0, 150,
+          0, 150,
+          30, 0,
+          30, 150,
+
+          // top rung
+          30, 0,
+          100, 0,
+          30, 30,
+          30, 30,
+          100, 0,
+          100, 30,
+
+          // middle rung
+          30, 60,
+          67, 60,
+          30, 90,
+          30, 90,
+          67, 60,
+          67, 90,
+      ]),
+      gl.STATIC_DRAW);
+}
+
+main();
